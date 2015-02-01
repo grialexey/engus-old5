bd13c700af7c7ca7c62bd8a29aceb1dad8d60fd0
Alexey Grigoriev <grialexey@gmail.com>
2015-02-01 21:38:51 +0300

    Admin perfomance optimization

--- engus/cards/admin.py
+++ engus/cards/admin.py
@@ -7,12 +7,17 @@ class CardFrontAdmin(admin.ModelAdmin):
     list_display = ('text', 'is_public', 'pronunciation', 'audio', 'author', 'created', )
     list_filter = ('is_public', )
     search_fields = ('text', )
+    raw_id_fields = ('author', )
 
 
 class CardAdmin(admin.ModelAdmin):
-    raw_id_fields = ['front', ]
+    raw_id_fields = ('front', 'learner', 'parent', )
+
+
+class DeckAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author', )
 
 
 admin.site.register(CardFront, CardFrontAdmin)
 admin.site.register(Card, CardAdmin)
-admin.site.register(Deck)
+admin.site.register(Deck, DeckAdmin)
