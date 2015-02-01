40b252b37319f37f42a41a207ea987c050575278
Alexey Grigoriev <grialexey@gmail.com>
2015-02-01 15:30:41 +0300

    Change admin interface for Card and CardFront models

--- engus/cards/admin.py
+++ engus/cards/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from .models import CardFront, Card, Deck
 
 
-admin.site.register(CardFront)
-admin.site.register(Card)
+class CardFrontAdmin(admin.ModelAdmin):
+    list_display = ('text', 'pronunciation', 'audio', )
+
+
+class CardAdmin(admin.ModelAdmin):
+    raw_id_fields = ['front', ]
+
+
+admin.site.register(CardFront, CardFrontAdmin)
+admin.site.register(Card, CardAdmin)
 admin.site.register(Deck)
