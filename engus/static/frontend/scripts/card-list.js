19b93c2b0c803aa38f98690bb551d31248d3199d
Alexey Grigoriev <grialexey@gmail.com>
2015-02-02 16:42:05 +0300

    Enhancements for pagination

--- engus/static/frontend/scripts/card-list.js
+++ engus/static/frontend/scripts/card-list.js
@@ -4,14 +4,18 @@ $(document).ready(function() {
         $content = $cardList.find('.card-list__content');
     $cardList.on('click', '.pages__link', function(event) {
         event.preventDefault();
-        var $link = $(this);
+        var $link = $(this),
+            url = $link.attr('href');
         $overlay.show();
         $.ajax({
-            url: $link.attr('href'),
+            url: url,
             method: 'get'
         }).done(function(data) {
             $overlay.hide();
             $content.html(data);
+            if(url != window.location){
+                window.history.pushState({ path:url }, '', url);
+            }
         });
     })
 });
