a4955605beac9b1190d5b5409120f9b6fe60dc34
Alexey Grigoriev <grialexey@gmail.com>
2015-02-02 16:31:04 +0300

    Fix one more bug with overlay when pages loading

--- engus/static/frontend/scripts/card-list.js
+++ engus/static/frontend/scripts/card-list.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
-    var $cardList = $('.card-list');
+    var $cardList = $('.card-list'),
+        $overlay = $cardList.find('.card-list__overlay'),
+        $content = $cardList.find('.card-list__content');
     $cardList.on('click', '.pages__link', function(event) {
         event.preventDefault();
-        var $link = $(this),
-            $overlay = $cardList.find('.card-list__overlay');
+        var $link = $(this);
         $overlay.show();
         $.ajax({
             url: $link.attr('href'),
             method: 'get'
         }).done(function(data) {
             $overlay.hide();
-            $cardList.html(data);
+            $content.html(data);
         });
     })
 });
