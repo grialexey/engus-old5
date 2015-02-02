d03f7578b83b905e5e966b1918bbf38413466c97
Alexey Grigoriev <grialexey@gmail.com>
2015-02-02 16:26:42 +0300

    Fix bug with overlay when loading pages

--- engus/static/frontend/scripts/card-list.js
+++ engus/static/frontend/scripts/card-list.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $cardList.on('click', '.pages__link', function(event) {
         event.preventDefault();
         var $link = $(this),
-            $overlay = $cardList.find('card-list__overlay');
+            $overlay = $cardList.find('.card-list__overlay');
         $overlay.show();
         $.ajax({
             url: $link.attr('href'),
