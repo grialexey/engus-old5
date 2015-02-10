c04d22f4b09fe5faf90dc5b0d26363fef151b9f8
Alexey Grigoriev <grialexey@gmail.com>
2015-02-10 23:24:50 +0300

    Migration for Artcle model

---
+++ engus/articles/migrations/0008_auto_20150211_0023.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('articles', '0007_auto_20150210_1724'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='article',
+            options={'ordering': ['-rating', '-published'], 'verbose_name': '\u0421\u0442\u0430\u0442\u044c\u044f', 'verbose_name_plural': '\u0421\u0442\u0430\u0442\u044c\u0438'},
+        ),
+    ]
