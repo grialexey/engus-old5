7846354ef938853f02d4ea5d5f4c88a769b6af1a
Alexey Grigoriev <grialexey@gmail.com>
2015-02-13 20:16:19 +0300

    Random repeat mode when click on cards to repeat count

--- engus/cards/context_processors.py
+++ engus/cards/context_processors.py
@@ -6,4 +6,7 @@ def cards_to_repeat_count(request):
         cards_count = Card.objects.filter(user=request.user).to_repeat().count()
     else:
         cards_count = 0
-    return {'cards_to_repeat_count': cards_count, }
+    return {
+        'cards_to_repeat_count': cards_count,
+        'card_repeat_modes': ['repeat-right', 'repeat-left', ]
+    }
