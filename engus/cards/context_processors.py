39006e91ccff2cebab8114574bbf8339709a9a61
Alexey Grigoriev <grialexey@gmail.com>
2015-02-18 11:01:44 +0300

    Little refactoring

--- engus/cards/context_processors.py
+++ engus/cards/context_processors.py
@@ -2,10 +2,9 @@ from .models import Card
 
 
 def cards_to_repeat_count(request):
+    cards_count = 0
     if request.user.is_authenticated():
         cards_count = Card.objects.filter(user=request.user).to_repeat().count()
-    else:
-        cards_count = 0
     return {
         'cards_to_repeat_count': cards_count,
         'card_repeat_modes': ['repeat-right', 'repeat-left', ]
